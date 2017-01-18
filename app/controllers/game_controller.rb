84f812b60e3f84acaa9a95c63d68c3c436f0e1ee
K-Wei <kevinwei@users.noreply.github.com>
2017-01-18 17:35:30 -0600

    completed rock paper scissors

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -9,12 +9,60 @@ class GameController < ApplicationController
 
     # Your logic here
 
+    rand_num = rand(3)
+    cpu_move = nil
+
+    if rand_num == 0
+      cpu_move = "rock"
+    elsif rand_num == 1
+      cpu_move = "paper"
+    else
+      cpu_move = "scissors"
+    end
+
+    if @user_move = "rock"
+      if cpu_move == "rock"
+        @result  = "tie!"
+      end
+      if cpu_move == "paper"
+        @result = "lost"
+      end
+      if cpu_move == "scissors"
+        @result = "won"
+      end
+    end
+
+    if @user_move = "paper"
+      if cpu_move == "rock"
+        @result = "won"
+      end
+      if cpu_move == "paper"
+        @result = "tie!"
+      end
+      if cpu_move == "scissors"
+        @result = "lost"
+      end
+    end
+
+    if @user_move = "scissors"
+      if cpu_move == "rock"
+        @result = "lost"
+      end
+      if cpu_move == "paper"
+        @result = "won"
+      end
+      if cpu_move == "scissors"
+        @result = "tie"
+      end
+    end
+
+
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    @computer_move = cpu_move
 
-    @result = "Replace this string with the correct value."
+    # @result = outcome
 
     # ===============================================================
     # Your code goes above.
